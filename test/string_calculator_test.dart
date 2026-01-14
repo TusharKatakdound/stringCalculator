9a1a3718de76d7fb31b5e171137503e6930f5d6c
Tushar Katakdound <tushar02.katakdound@gmail.com>
2026-01-14 14:32:28 +0530

    test: return 0 when input string is empty

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+import '../lib/string_calculator.dart';
+
+void main() {
+  group('String Calculator TDD', () {
+    test('empty string returns 0', () {
+      final calculator = StringCalculator();
+      expect(calculator.add(''), 0);
+    });
+  });
+}
